76ab84d2d21761abdc6cb560a9946a12a42f09c7
eugeneoyier <eugeneoyier@gmail.com>
2025-05-25 09:36:17 +0000

    I love C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
 const count = parseInt(process.argv[2], 10);
 
-if (isNaN(count) || count <= 0) {
+if (isNaN(count)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (count > 0) {
   let i = 0;
   while (i < count) {
     console.log("C is fun");
