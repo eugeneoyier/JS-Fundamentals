6bdb7c9721b065279d7625be366d7f25273d40a4
eugeneoyier <eugeneoyier@gmail.com>
2025-05-25 09:28:28 +0000

    Loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+let output = "";
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i] + "\n";
+}
+
+console.log(output.trim());
